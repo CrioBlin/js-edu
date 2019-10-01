6fcec0f6ed02276656547534a30c164005619d10
CrioBlin <crioblin11@gmail.com>
2019-10-01 11:49:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      let weeks = 0;
+      
+      if(knowsProgramming) 
+        weeks = 800 / config[focus];
+      else
+        weeks = 800 / config[focus] + 500 / config[focus];
+    
+      return Math.ceil(weeks);
   };
   
